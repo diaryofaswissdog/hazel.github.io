92eb490d49d32108223a62bc0af1c837332cdbd6
diaryofaswissdog <146657537+diaryofaswissdog@users.noreply.github.com>
2023-10-01 19:19:21 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('navbar').innerHTML=`
+<a href="webseite.html">Home</a>
+<a href="mycv.html">My CV</a>
+<a href="hazelpreferences.html">More about me</a>
+<a href="help.html">Get active</a>
+`;
